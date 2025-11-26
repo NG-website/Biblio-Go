308b633e1bf05f96230255c472c719a76d636f76
justnico83 <afec.nicolas@gmail.com>
2025-11-26 01:25:47 +0100

    ssl

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
+import fs from 'fs'
 dotenv.config()
 
 const sequelize = new Sequelize(
@@ -8,13 +9,22 @@ const sequelize = new Sequelize(
   process.env.PASSWORD_DB,
   {
     host: process.env.HOST_DB,
-    dialect: process.env.DIALECT_DB
+    dialect: process.env.DIALECT_DB,
+    dialectOptions: {
+    ssl: {
+      require: true,
+      ca: fs.readFileSync("certs/cert.pem"), // chemin relatif vers ton certificat
+      // rejectUnauthorized: false // optionnel si certificat auto-signé
+    }
+  },
+    logging:false
   }
 )
-// try {
-//   await sequelize.authenticate();
-//   console.log('Connection db ok');
-// } catch (error) {
-//   console.error('Erreur connection db', error);
-// }
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection db ok');
+} catch (error) {
+  console.error('Erreur connection db', error);
+}
 export default sequelize
