af37512bf6cd0a5d7c04193cf95b14d37de80b0a
justnico83 <afec.nicolas@gmail.com>
2025-12-08 12:42:55 +0100

    index

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    httpOnly: true,
+    //httpOnly: true,
      secure: true,
      sameSite: "none",
    // maxAge: 1000 * 60 * 60 * 24 * 7
