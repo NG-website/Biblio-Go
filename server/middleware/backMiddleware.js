a1c463a3493833fd9e153a3c320926a0dd524ada
justnico83 <afec.nicolas@gmail.com>
2025-12-15 17:42:13 +0100

    backmidd

--- server/middleware/backMiddleware.js
+++ server/middleware/backMiddleware.js
@@ -1,19 +1,20 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt'
 const backMiddleware = async (req, res, next) => {
-    
-    const token = req.headers.authorization
-    console.log(req.headers.origin)
-    const decode = jwt.decode(token)
-    const expired = decode.token < new Date()
-    if(!expired){
-        res.status(403).json("token expiré")
-    }
-   
+
+
+
     if (req.headers.origin == "https://biblio-go.vercel.app") {
         console.log("front url ok")
         next()
     } else {
+        const token = req.headers.authorization?.split(' ')[1]
+        console.log(req.headers.origin)
+        const decode = jwt.decode(token)
+        const expired = decode.exp * 1000 < new Date()
+        if (!expired) {
+            res.status(403).json("token expiré")
+        }
         if (token && decode) {
             console.log("devrait pas etre la")
             fetch(`${process.env.API_URL}api/auth/user`, {
@@ -23,14 +24,14 @@ const backMiddleware = async (req, res, next) => {
             })
                 .then((res) => { return res.json() })
                 .then((data) => {
-                    if(data){
-                         const isValide = bcrypt.compare("", data.token)
-                    if (isValide) {
-                        next()
-                    }else{
-                    res.status(403).json("token non valid")
+                    if (data) {
+                        const isValide = await bcrypt.compare("", data.token)
+                        if (isValide) {
+                            next()
+                        } else {
+                            res.status(403).json("token non valid")
+                        }
                     }
-                }
                 })
         }
         res.status(500).json("url unautorized")
